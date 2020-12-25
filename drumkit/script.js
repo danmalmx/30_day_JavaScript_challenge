d272f6ef0372b13c66b8a043b6c8e35598c9afa0
Daniel Malmgren de Oliveira <danmalmx@gmail.com>
2020-12-25 17:26:29 +0100

    Add functionality for a drumkit

---
+++ drumkit/script.js
@@ -0,0 +1,25 @@
+window.addEventListener('keydown', playSound);
+
+function playSound(e) {
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+    if (!audio) {
+        return
+    } else {
+        audio.currentTime = 0;
+        audio.play();
+    }
+    key.classList.add('playing');
+};
+function removeTransition(e) {
+    if (e.propertyName !== 'transform') {
+        return;
+    }else {
+        this.classList.remove('playing')
+        console.log(e.propertyName);
+
+    }
+}
+
+const keys = document.querySelectorAll('.key');
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
