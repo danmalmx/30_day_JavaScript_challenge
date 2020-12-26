f3f793f225cfea90674f47ff5cde6a73506a3ef7
Daniel Malmgren de Oliveira <danmalmx@gmail.com>
2020-12-26 21:00:40 +0100

    Add minutes and hours  pointer functionality

--- css_js_clock/script.js
+++ css_js_clock/script.js
@@ -1,11 +1,25 @@
 const secondHand = document.querySelector('.second-hand');
+const minHand = document.querySelector('.min-hand')
+const hourHand = document.querySelector('.hour-hand')
 
 function setDate() {
     const now = new Date();
     const seconds = now.getSeconds();
+    const minutes = now.getMinutes();
+    const hours = now.getHours();
 
     const secondsDegree = ((seconds / 60) * 360) + 90;
+    const minutesDegree = ((minutes / 60) * 360) + 90;
+    const hoursDegree = ((hours / 12) * 360) +90;
+
+
     secondHand.style.transform = `rotate(${secondsDegree}deg)`;
+    minHand.style.transform = `rotate(${minutesDegree}deg)`;
+    hourHand.style.transform = `rotate(${hoursDegree}deg)`;
+    
+    if(seconds === 59) {
+        secondHand.style.transition = null;
+    }
     console.log(seconds);
 
     
