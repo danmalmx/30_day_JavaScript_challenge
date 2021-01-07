94ca288f0a1f6cc59e8a6798348c136bb8270e80
Daniel Malmgren de Oliveira <danmalmx@gmail.com>
2021-01-07 20:22:48 +0100

    Add functionality to check for which keys were entered and add cornify image if equal to secret code

--- Key_Sequence/script.js
+++ Key_Sequence/script.js
@@ -1,16 +1,12 @@
 const pressed = [];
-const secretCode = 'danpel';
+const secretCode = 'unicorns';
 
 
 window.addEventListener('keyup', (e) => {
-    console.log(e);
     pressed.push(e.key)
     pressed.splice(-secretCode.length -1, pressed.length - secretCode.length )
     
     if (pressed.join('').includes(secretCode)) {
         cornify_add();
-        console.log('Yaaaaaas');
     }
-
-    console.log(pressed);
 })
