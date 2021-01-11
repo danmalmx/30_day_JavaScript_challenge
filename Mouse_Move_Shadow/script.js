8e51027846c13895f37dd2dad9c0d10a86d21e57
Daniel Malmgren de Oliveira <danmalmx@gmail.com>
2021-01-11 20:52:57 +0100

    Add shadow function

--- Mouse_Move_Shadow/script.js
+++ Mouse_Move_Shadow/script.js
@@ -4,12 +4,22 @@ const text = document.querySelector('h1');
 function shadow(e) {
     const { offsetWidth: width, offsetHeight: height } = hero;
     let { offsetX: x, offsetY: y} = e;
+    const distance = 100;
 
     if (this !== e.target) {
         x = x + e.target.offsetLeft;
         y = y + e.target.offsetTop;
     }
-    console.log(x, y);
+
+    const xDistance = (x / width * distance) - (distance / 2)
+    const yDistance = (y / height * distance) - (distance / 2)
+    
+    text.style.textShadow = `
+    ${xDistance}px ${yDistance}px 0 red,
+    ${xDistance *-1}px ${yDistance}px 0 blue
+    
+    `
+    console.log(xDistance, yDistance);
 
 }
 
