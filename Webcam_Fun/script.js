a78326134a4eb2838f83d0e88f562d4e003e7517
Daniel Malmgren de Oliveira <danmalmx@gmail.com>
2021-01-16 20:21:14 +0100

    Add functionality for redScreen, rgbSplit or greenScreen (default)

--- Webcam_Fun/script.js
+++ Webcam_Fun/script.js
@@ -22,7 +22,13 @@ function paintToCanvas() {
     canvas.height = height;
 
     setInterval(() => {
-        ctx.drawImage(video, 0, 0, width, height)
+        ctx.drawImage(video, 0, 0, width, height);
+        let pixels = ctx.getImageData(0, 0, width, height)
+        // pixels = redEffect(pixels);
+        // pixels = rgbSplit(pixels);
+        // ctx.globalAlpha = 0.1;
+        pixels = greenScreen(pixels);
+        ctx.putImageData(pixels, 0, 0);
     }, 16);
 }
 
@@ -34,12 +40,54 @@ function takePhoto() {
     const link = document.createElement('a');
     link.href = data;
     link.setAttribute('download', 'handsome');
-    link.textContent = "Download image";
+    link.innerHTML = `<img src=${data} alt="Beautiful person" </img>`;
     strip.insertBefore(link, strip.firstChild)
     console.log(data);
 }
 
+function redEffect(pixels) {
+    for(let i = 0; i < pixels.data.length; i += 4) {
+        pixels.data[i + 0] += 100;
+        pixels.data[i + 1] -= 50;
+        pixels.data[i + 2] *= 0.5;
+    }
+    return pixels;
+}
 
+function rgbSplit(pixels) {
+    for(let i = 0; i < pixels.data.length; i += 4) {
+        pixels.data[i - 150] = pixels.data[i + 0];
+        pixels.data[i + 300] = pixels.data[i + 1]
+        pixels.data[i - 350] = pixels.data[i + 2]
+    }
+    return pixels;
+}
+
+function greenScreen(pixels) {
+    const levels = {};
+
+    document.querySelectorAll('.rgb input').forEach((input) => {
+        levels[input.name] = input.value;
+    });
+
+    for (let i = 0; i < pixels.data.length; i += 4) {
+        red = pixels.data[i + 0];
+        green = pixels.data[i + 1];
+        blue = pixels.data[i + 2];
+        alpha = pixels.data[i + 3];
+
+        if(red >= levels.rmin
+            && green >= levels.gmin
+            && blue >= levels.bmin
+            && red <= levels.rmax
+            && green <= levels.gmax
+            && blue <= levels.bmax) {
+
+                pixels.data[i + 3] = 0
+            }
+    }
+    return pixels;
+}
 getVideo();
 
 video.addEventListener('canplay', paintToCanvas);
