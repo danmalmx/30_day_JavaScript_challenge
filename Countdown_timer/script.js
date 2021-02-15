fb5d0b2461c93a198eb7136a4174b8d4bd775094
Daniel Malmgren de Oliveira <danmalmx@gmail.com>
2021-02-15 20:37:41 +0100

    Adding basic timer function

--- Countdown_timer/script.js
+++ Countdown_timer/script.js
@@ -1,8 +1,20 @@
+let cuntdown;
+
 function timer(sec) {
   const now = Date.now();
   const then = now + (sec * 1000)
-  setInterval(() => {
+  displayTmeLeft(sec);
+  countdown = setInterval(() => {
     const secLeft = Math.round((then - Date.now()) / 1000);
-    console.log(secLeft);
+
+    if(secLeft === 0) {
+      clearInterval(countdown);
+      return;
+    }
+    displayTmeLeft(secLeft);
   }, 1000);
+}
+
+function displayTmeLeft(sec) {
+  console.log(sec);
 }
