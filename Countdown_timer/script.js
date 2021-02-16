fd1b21da480649217ad26529b8ecbf300800734e
Daniel Malmgren de Oliveira <danmalmx@gmail.com>
2021-02-16 19:38:53 +0100

    Add functioanlity to show end time together with countdown

--- Countdown_timer/script.js
+++ Countdown_timer/script.js
@@ -1,10 +1,12 @@
 let cuntdown;
 const timerDisplay = document.querySelector('.display__time-left');
+const endTime = document.querySelector('.display__end-time');
 
 function timer(sec) {
   const now = Date.now();
   const then = now + (sec * 1000)
   displayTimeLeft(sec);
+  displayEndTime(then)
   countdown = setInterval(() => {
     const secLeft = Math.round((then - Date.now()) / 1000);
 
@@ -22,4 +24,11 @@ function displayTimeLeft(sec) {
   const display = `${min}:${remainSec < 10 ? 0 : ''}${remainSec}`;
   document.title = display;
   timerDisplay.textContent = display;
+}
+
+function displayEndTime(timestamp) {
+  const end = new Date(timestamp);
+  const hour = end.getHours();
+  const min = end.getMinutes();
+  endTime.textContent = `Be back at ${hour}: ${min}`
 }
